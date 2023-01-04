654f83de4fba86195a52ac4d730ac0d8771aa645
piratefresh <magnussithnilsen@gmail.com>
2023-01-04 02:28:25 -0500

    missing modules

--- packages/ui/src/DatePicker/CalendarTriggerButton.tsx
+++ packages/ui/src/DatePicker/CalendarTriggerButton.tsx
@@ -1,5 +1,4 @@
 import { CalendarIcon } from "@heroicons/react/24/outline";
-import { CalendarOutline24Icon } from "@vygruppen/spor-icon-react";
 import React from "react";
 import { AriaButtonProps, useButton } from "react-aria";
 import { Button } from "../Button";
