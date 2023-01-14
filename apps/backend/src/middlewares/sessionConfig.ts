32d2342fee8e2642cf2d0df096c0021ee5372d4f
piratefresh <magnussithnilsen@gmail.com>
2023-01-13 23:28:49 -0500

    Update sessionConfig.ts

--- apps/backend/src/middlewares/sessionConfig.ts
+++ apps/backend/src/middlewares/sessionConfig.ts
@@ -14,11 +14,12 @@ export const sessionMiddleware = session({
   }),
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-    httpOnly: true,
+    httpOnly: false,
     secure: __prod__,
     sameSite: "lax",
   },
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  proxy: true,
 });
