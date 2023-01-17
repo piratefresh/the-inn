f1d09899197a9af11ab543db227043bdd19f4a0a
piratefresh <magnussithnilsen@gmail.com>
2023-01-17 12:21:57 -0500

    Delete prismadb.ts

--- apps/web/lib/prismadb.ts
+++
@@ -1,9 +0,0 @@
-import * as prismaBackend from "backend";
-
-declare global {
-  var prisma: prismaBackend.PrismaClient | undefined;
-}
-
-export const prisma = global.prisma || new prismaBackend.PrismaClient();
-
-if (process.env.NEXT_PUBLIC_VERCEL_ENV !== "production") global.prisma = prisma;
