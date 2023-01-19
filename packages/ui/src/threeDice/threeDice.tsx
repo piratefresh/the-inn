19fb7c6140d38e646a17f8dafae1e764beefb923
piratefresh <magnussithnilsen@gmail.com>
2023-01-19 11:45:37 -0500

    3d init

--- packages/ui/src/threeDice/threeDice.tsx
+++ packages/ui/src/threeDice/threeDice.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Mesh } from "three";
-import { Canvas, useFrame, extend } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import {
+  Canvas,
+  useFrame,
+  extend,
+  useThree,
+  Object3DNode,
+} from "@react-three/fiber";
+import { MeshReflectorMaterial, OrbitControls, Text } from "@react-three/drei";
 
-extend({ OrbitControls });
+import { CustomObject } from "./CustomObject";
+
+const cameraSettings = {
+  fov: 45,
+  near: 0.1,
+  far: 500,
+  position: [3, 2, 6],
+};
 
 export const ThreeDice = () => {
   return (
-    <Canvas>
+    <Canvas orthographic gl={{ antialias: true }} camera={cameraSettings}>
       <Experience />
     </Canvas>
   );
@@ -16,18 +29,20 @@ export const ThreeDice = () => {
 export const Experience = () => {
   const groupRef = React.useRef<Mesh>(null!);
   const cubeRef = React.useRef<Mesh>(null!);
+  const { camera, gl } = useThree();
   useFrame((state, delta) => {
-    console.log("tick");
     if (cubeRef.current) cubeRef.current.rotation.y += delta;
-    if (groupRef.current) groupRef.current.rotation.y += delta;
+    // if (groupRef.current) groupRef.current.rotation.y += delta;
   });
   return (
     <>
-      <OrbitControls />
+      <OrbitControls makeDefault />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
       <group ref={groupRef}>
         <mesh position-x={-2}>
           <sphereGeometry />
-          <meshBasicMaterial color="orange" />
+          <meshStandardMaterial color="orange" />
         </mesh>
         <mesh
           ref={cubeRef}
@@ -36,14 +51,19 @@ export const Experience = () => {
           scale={1.5}
         >
           <boxGeometry scale={1.5} />
-          <meshBasicMaterial color="mediumpurple" />
+          <meshStandardMaterial color="mediumpurple" />
         </mesh>
       </group>
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshBasicMaterial color="greenyellow" />
+        {/* <meshBasicMaterial color="greenyellow" /> */}
+        <MeshReflectorMaterial mirror={0.75} />
       </mesh>
+
+      <Text fontSize={1} color="salmon" position-y={2}>
+        I Love R3F
+      </Text>
     </>
   );
 };
